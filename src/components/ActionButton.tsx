8e78c2543c7a4ca2f9a95bcb83d0f56844a1e9fa
Mônica Colomera <colomeramonica@gmail.com>
2024-06-06 11:41:30 -0300

    updating dependencies

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -1,9 +1,9 @@
-import { Check, Close } from "@mui/icons-material"
-import { Button } from "@mui/material"
 import React, { useEffect, useState } from "react"
-
+import { ReactComponent as CloseIcon } from '../assets/icons/close.svg'
+import { ReactComponent as CheckIcon } from '../assets/icons/check.svg'
+import { Button } from "@mui/material";
 export const ActionButton: React.FC<{ type: string }> = ({ type }) => {
-  const [buttonIcon, setButtonIcon] = useState(<Check />);
+  const [buttonIcon, setButtonIcon] = useState(<CheckIcon />);
 
   useEffect(() => {
     setButtonStyle();
@@ -11,10 +11,10 @@ export const ActionButton: React.FC<{ type: string }> = ({ type }) => {
 
   const setButtonStyle = () => {
     if (type === 'decline') {
-      return setButtonIcon(<Close />)
+      return setButtonIcon(<CloseIcon style={{ width: '20px', height: '30px', fill: '#FFFFFF' }} />)
     }
 
-    return setButtonIcon(<Check />)
+    return setButtonIcon(<CheckIcon style={{ width: '20px', height: '30px', fill: '#FFFFFF' }} />)
   }
 
   return (
